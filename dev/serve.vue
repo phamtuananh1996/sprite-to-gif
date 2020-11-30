69e1e50d914b137e9e50bf82250d4be9d429c060
anhpt <pham.tuan.anh@sun-asterisk.com>
2020-12-01 00:40:08 +0700

    first commit

---
+++ dev/serve.vue
@@ -0,0 +1,26 @@
+<script>
+import Vue from 'vue';
+import { SpriteToGif } from '@/entry';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    SpriteToGif,
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <sprite-to-gif 
+      image="https://images.viblo.asia/a28b8126-1298-4ca7-a507-53865b32238d.png"
+      :width="100"
+      :height="100"
+      :column="7"
+      :row="4"
+      :max="27"
+      :frame="20"
+      :loop="10"
+    />
+  </div>
+</template>
---
+++ src/lib-components/sprite-to-gif.vue
@@ -0,0 +1,64 @@
+<template>
+  <div :style="style">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: String,
+    height: Number,
+    width: Number,
+    loop: Number,
+    frame: Number,
+    max: Number,
+    column: Number,
+    row: Number,
+  },
+  mounted() {
+    this.play()
+  },
+  data(){
+    return {
+      style: {
+        width: this.width + 'px',
+        height: this.height + 'px',
+        background: `url(${this.image})`,
+        backgroundSize: `${this.width * this.column}px ${this.height * this.row}px`,
+        backgroundPosition: '0px 0px'
+      }
+    }
+  },
+  
+  methods: {
+    play() {
+      let i = 0
+      let position = {
+        x: 0,
+        y: 0,
+        loop: 0,
+      }
+      const playTimer = setInterval(() => {
+        i++;
+        if(i % this.column) {
+          position.x -= this.width; 
+        } else {
+          position.y -= this.height; 
+          position.x = 0;
+        }
+        if (i == this.max) {
+          i = 0
+          position.y = 0; 
+          position.x = 0;
+          position.loop++;
+          if(position.loop >= this.loop) {
+            clearInterval(playTimer)
+          }
+        }
+        
+        this.$set(this.style, 'backgroundPosition', `${position.x}px ${position.y}px`)
+      }, 1000/this.frame);
+    }
+  }
+}
+</script>
